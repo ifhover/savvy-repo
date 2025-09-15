9fae53a88caa6ec989f91fce40cd6214cc82fac7
sudden3 <sudden3@foxmail.com>
2025-09-15 09:51:29 +0800

    add dist in .gitignore

--- apps/server/dist/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-import { createMiddleware } from "hono/factory";
-export const auth = createMiddleware(async (c, next) => {
-    const token = c.req.header("Authorization");
-    if (!token) {
-        return c.json({ error: "Unauthorized" }, 401);
-    }
-    await next();
-});
--- apps/server/dist/modules/user/userService.js
+++
@@ -1,6 +0,0 @@
-export const userService = {
-  getUserInfo: async (c) => {
-    const info = {};
-    return success(info, "Hello World");
-  },
-};
